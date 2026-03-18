e20aacb9a3f816ba942fba92afbbe414cae2a54c
Rixty9 <atharvapandit84@gmail.com>
2026-03-18 18:41:27 +0530

    Assignment 2

--- BATCH_5_STUDENTS/PritamSutar_01/JAVASCRIPT/JS_ASSIGNMENTS/02_Assignment.js
+++ BATCH_5_STUDENTS/PritamSutar_01/JAVASCRIPT/JS_ASSIGNMENTS/02_Assignment.js
@@ -3,5 +3,22 @@
  to assign values to them and update to them
  */
 
- let num=10;
- console.log("num");
+ let num = 10;
+ console.log(num)
+
+ num = 45
+ console.log(num)
+
+ var sum = 56
+ console.log(sum)
+
+ var number = 100
+
+console.log(number)
+
+const user = 10
+console.log(user)
+
+ /*const user = 189
+ 
+ console.log(user)*/
