e4b5cfeaf8f19f895fd9dbeb51b3837eab8927f7
aashiya7866 <aashiya.khan8377@gmail.com>
2026-03-18 21:24:23 +0530

    Assignment No 02

---
+++ BATCH_5_STUDENTS/Aashiya/JAVASCRIPT/JS_ASSIGNMENTS/02_AK_AssignmentNo_2.js
@@ -0,0 +1,33 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/*omments can make code readable, easy to reuse and informative
+*/
+
+let variabl1
+let variable2
+var variable3
+var variable4
+
+
+
+let food ="Biryani"
+let pet="cat"
+var profession="Automation_engineer"
+var hometown="pune"
+
+
+
+const first_Name="Aashiya"
+const last_Name="khan"
+const country="India"
+let marital_status="Married"
+let age="30"
+
+const first_Name1 = "Aashiya", last_Name1 = "khan", country1 = "India", marital_status1 = "Married", age1 = "30";
+
+let myAge="30"
+let yourAge="20"
+
+console.log(myAge)
+console.log(yourAge)
