b02f68a38d33a7220409bd0381ded3451b9fb561
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-03-18 22:17:11 +0530

    Assignment 2 -variables

---
+++ JAVASCRIPT/JAVASCRIPT_ASSIGNMENTS/02_b_ASSIGNMENT.js
@@ -0,0 +1,6 @@
+// Create declare variables and assign string, boolean, undefined and null data types
+
+
+
+
+// Create use the JavaScript typeof operator to check different data types. Check the data type of each variable
