808efb7401d4b0ab969c91e44092950cf8485881
shivani <shvanikshirsagar2204@gmail.com>
2026-03-18 11:34:40 -0700

    assignment 2

---
+++ BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/02_shivaniassignment.js
@@ -0,0 +1,72 @@
+// Write a single line comment which says, comments can make code readable
+// these are the data which we will be using in our assignment
+
+
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// lets started to learn about javascripts
+
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* this 
+is the 
+most basic and
+important
+concept of js */
+
+
+
+// Declare four variables without assigning values
+let colour;
+const day = "monday";
+var marks;
+let movie;
+
+
+
+
+// wright four variable with assigning values
+
+let name = "shivani"
+console.log(name)
+
+
+let tree = "mango tree"
+console.log(tree)
+
+
+const country = "india"
+console.log(country)
+
+
+var car = "punch"
+console.log(car)
+
+car = "safary"
+console.log(car)
+
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let Firstname = "shivani"
+let Lastname = "kshirsagar-sarkale"
+let Maritalstatus = "married"
+let Country = "india"
+let Age = 27
+
+
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let Firstname_1 = "shivani", Lastname_1 = "kshirsagar-sarkale", Maritialstatus_1 = "married", Country_1 = "India", Age_1 = 27
+
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myage = 28
+console.log(myage)
+
+let yourage = 35
+console.log(yourage)
+
