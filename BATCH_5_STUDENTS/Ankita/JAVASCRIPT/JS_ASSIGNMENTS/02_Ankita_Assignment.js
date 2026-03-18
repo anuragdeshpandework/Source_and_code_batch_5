9ecd12c400d7a0e5ccc6f831015bc4b7e6be4e9d
Ankita Fartade <ankitafartde.work@gmail.com>
2026-03-18 15:45:29 +0530

    02 Assignmnet Completed

---
+++ BATCH_5_STUDENTS/Ankita/JAVASCRIPT/JS_ASSIGNMENTS/02_Ankita_Assignment.js
@@ -0,0 +1,54 @@
+// Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+Comments can make code readable,
+east tp reuse
+and informative
+
+*/
+
+// Declare four variables without assigning values
+let schoolName
+let fullName
+let pi
+let balance
+
+// Declare four variables with assigned value
+let color = "Pink"
+console.log("color=", color)
+let place = "Vrindavan"
+console.log("place=", place)
+let siblingCount = 2
+console.log("siblingCount=", siblingCount)
+let middleName = "Hanumant"
+console.log("middleName=", middleName)
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Ankita"
+console.log("First Name=", firstName)
+let lastName = "Fartade"
+console.log("Last Name=", lastName)
+let maritalStatus = "unmarried"
+console.log("Marital Status =", maritalStatus)
+let country = ("India")
+console.log("Country=", country)
+let age = 23
+console.log("Age=", age)
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName1 = "Ankita"
+let lastName1 = "Fartde"
+let maritalStatus1 = "unmarried"
+let country1 = "India"
+let age1 = 23
+console.log(firstName1, lastName1, maritalStatus1, country1, age1)
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myAge = 23
+let yourAge = 26
+console.log("My Age=", myAge, "Your Age=", yourAge)
---
+++ JAVASCRIPT/JAVASCRIPT_ASSIGNMENTS/02_ASSIGNMENT.js
@@ -0,0 +1,20 @@
+// Write a single line comment which says, comments can make code readable
+
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+
+// Declare four variables without assigning values
+
+// Declare four variables with assigned value
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
