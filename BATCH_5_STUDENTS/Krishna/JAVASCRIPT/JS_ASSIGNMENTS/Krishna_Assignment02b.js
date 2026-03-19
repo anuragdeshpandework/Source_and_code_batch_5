4c3d3fb38082390b2ade1e767f6aed5814581455
Krishna04 <krishnajkadam.work@gmail.com>
2026-03-19 19:26:49 +0530

    Assignment02b

---
+++ BATCH_5_STUDENTS/Krishna/JAVASCRIPT/JS_ASSIGNMENTS/Krishna_Assignment02b.js
@@ -0,0 +1,49 @@
+// Create declare variables and assign string, boolean, undefined and null data types
+//String
+let variable1 = "mornig"
+let variable2 = 'afternoon'
+let variable3 = `         `
+//Boolean
+let element1 = true
+let element2 = false
+//Undefined
+let accountNumber = undefined
+let cifNumber 
+//null
+let accountNumber1 = null
+let transactionID = null
+
+
+// Create use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+//number
+let accountNumber2 = 123456
+console.log(accountNumber2)
+console.log(typeof accountNumber2)
+
+//string
+let variable = "Morning"
+console.log(variable)
+console.log(typeof variable)
+
+//boolean
+let element3 = true
+console.log(element3)
+console.log(typeof element3)
+
+let element4 = false
+console.log(element4)
+console.log(typeof element4)
+
+//undefined
+let cifNumbers 
+console.log(cifNumbers)
+console.log(typeof cifNumbers)
+
+//null
+let accountNumber123 = null
+console.log(accountNumber123)
+console.log(typeof accountNumber123)
+
+
+
