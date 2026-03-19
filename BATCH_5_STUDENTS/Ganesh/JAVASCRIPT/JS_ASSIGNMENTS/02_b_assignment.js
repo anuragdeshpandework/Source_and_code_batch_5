d18c04c3a872fe797c9512b9532d9b4f677416d3
Ganesh <gdhage86@gmail.com>
2026-03-19 19:31:14 +0530

    Assignment 02b submission

---
+++ BATCH_5_STUDENTS/Ganesh/JAVASCRIPT/JS_ASSIGNMENTS/02_b_assignment.js
@@ -0,0 +1,22 @@
+// Create declare variables and assign string, boolean, undefined and null data types
+let name = "a String in vaiable"
+let flag = true
+let futureVaule = undefined
+let emptyData = null
+
+
+
+
+// Create use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let day = "Sunday"
+console.log(typeof day)
+let date = 19
+console.log(typeof date)
+let flag2 = false
+console.log(typeof flag2)
+let futureVaule2 = undefined
+console.log(typeof futureVaule2)
+let futureVaule3
+console.log(typeof futureVaule3)
+let emptyData2 = null
+console.log(typeof emptyData2)
