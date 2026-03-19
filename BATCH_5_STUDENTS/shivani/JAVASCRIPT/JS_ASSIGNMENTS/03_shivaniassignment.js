7e844b6f3a0f017dda2132e2e3b5f98dd35d5932
shivani <shvanikshirsagar2204@gmail.com>
2026-03-19 11:32:48 -0700

    numbers

---
+++ BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/03_shivaniassignment.js
@@ -0,0 +1,41 @@
+// JavaScript Number Data Type - Assignment
+
+// Simple Questions:
+
+// 1. What will be the output of the following code? 
+let x = 7.2;
+console.log(Math.round(x));
+// 7
+
+
+
+// 2. Write code to check the type of the variable `gravity = 9.81` using the typeof operator.
+// Example:
+let gravity = 9.81;
+console.log(typeof gravity)
+// type of veriable = number
+
+
+
+// Medium Questions:
+
+// 3. Generate a random number between 1 and 10 (inclusive) and print it. (51 -100)
+// Hint: Use Math.random() and Math.floor() to generate random numbers.
+let randomNum = Math.floor(Math.random()*10)+1
+console.log(randomNum)
+
+
+
+// 4. What is the output of rounding the number 5.7 using Math.ceil() and Math.floor()? 
+let num = 5.7;
+// Hint: Rounds up to the nearest integer.
+ // Hint: Rounds down to the nearest integer.
+
+ console.log(Math.ceil(num)) //output=6
+ console.log(Math.floor(num)) //output =5
+
+
+
+ // 5. Write program  `rollDice` that simulates rolling two dice and returns their combined result.
+// Hint: Each dice roll should return a number between 1 and 6.
+
