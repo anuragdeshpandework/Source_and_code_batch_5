043e17016bdc8dfdad111c71a997928ee10e23fc
shivani <shvanikshirsagar2204@gmail.com>
2026-03-20 10:55:37 -0700

    pull

--- BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/03_shivaniassignment.js
+++ BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/03_shivaniassignment.js
@@ -34,6 +34,33 @@ let num = 5.7;
  console.log(Math.ceil(num)) //output=6
  console.log(Math.floor(num)) //output =5
 
-   
+     
 
 
+// 5. Write program  `rollDice` that simulates rolling two dice and returns their combined result.
+// Hint: Each dice roll should return a number between 1 and 6.
+
+
+// 6. What will be the result of the following code? Explain why.
+let randomVal = Math.random();
+console.log(Math.floor(randomVal * 10) + 5);
+// write the output and reason in commnets 
+
+// Hint: Understand how the number is being generated and what range it falls in.
+
+//7 generate  randomBetween(10, 20) should generate a number between 10 and 20.
+console.log(randomBetween(10, 20));
+
+// 8 generate a random number between 5.5-9.3
+//Example: randomFloat(5.5, 9.3) should generate a random float between 5.5 and 9.3.
+
+
+
+
+// 9. Given a number `x = 1234.56789`, write code to round this number to 2 decimal places. that is x=1234.57
+let number = 1234.56789;
+
+
+
+// 10. Write a program  that rounds any given number to the nearest multiple of 5.
+// Example:if 28 is given  should return 30, 32 should return 30.
