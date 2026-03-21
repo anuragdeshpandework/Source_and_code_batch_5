544cddf5c826f7a8bb51823e86365d1684702234
Dhananjay Ugwekar <dhananjayugwekar.work@gmil.com>
2026-03-21 19:14:08 +0530

    02_Assignment_02

--- BATCH_5_STUDENTS/Dhananjay/JAVASCRIPT/JS_ASSIGNMENTS/02_DU_Assignment_2.js
+++ BATCH_5_STUDENTS/Dhananjay/JAVASCRIPT/JS_ASSIGNMENTS/02_DU_Assignment_2.js
@@ -1,4 +1,4 @@
-// Assignment - 02_17.03.2026
+// Practice - 02_17.03.2026
 
 // Savings Account Balence using let
 let SABal = "10,0000/-"
@@ -17,3 +17,46 @@ console.log(FDBal)
 // Customer Name using const
 const CustomerName = "Virat Kohli"
 console.log(CustomerName)
+
+//Assignment - 2 - 17.03.2026
+
+/* Multiline Command
+"Comments can make code readable", 
+"easy to reuse",
+"Informative" */
+
+// Declare four variables without assigning values
+FirstName
+LastName
+MaritalStatus
+Country
+age
+
+
+// Declare four variables with assigned value
+FirstName = "Dhananjay"
+LastName = "Ugwekar"
+MaritalStatus = "Married"
+Country = "India"
+age = "29"
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let FirstName = "Dhananjay"
+let LastName = "Ugwekar"
+var MaritalStatus = "Married"
+var Country = "India"
+var age = "29"
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let FirstName = "Dhananjay", LastName = "Ugwekar", MaritalStatus = "Married", Country = "India", age = "29";
+console.log(FirstName,LastName,MaritalStatus,Country,age)
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myage = "30"
+console.log(myage)
+
+let yourAge = "35"
+console.log(yourAge)
