c36ad3ae87acbd7c11f6dbc88d83900a4357670e
shwtabholankarwork <shweta.b.work7@gmail.com>
2026-03-22 19:16:28 +0530

    02_SB_Assignment_02

---
+++ BATCH_5_STUDENTS/Shweta/JAVASCRIPT/JS_ASSIGNMENTS/02_SB_Assignment_02.js
@@ -0,0 +1,45 @@
+//Second asignment of javascript
+console.log("My script")
+
+//Welcome to 30Days of javascript
+
+/*Comments make can code readable,
+and also code easy to reuse,
+and give information*/
+
+//Declare four variable without assigning values
+let MyName
+let AdharNumber
+let PiValue
+let PanNumber
+
+//Declare four variable with Assigned values
+let place = "Trambkeshwar"
+console.log("places",place)
+let subject ="Math"
+console.log("subjects",subject)
+let colour = "Blue"
+console.log("colours",colour)
+let Arithmaticvalue ="Three"
+console.log("Arithmatics",Arithmatic)
+
+//Declare variables to store your first name,last name, maritial status,country and age in single
+let FirstName = "Shweta"
+console.log("First Name",firstName)
+let lastName = "Bholankar"
+console.log("last Name",lastname)
+let maritialstatus = "Married"
+console.log("MaritialStatus",maritialstatus)
+let country = "India"
+console.log("country",country)
+let age = "28"
+console.log("age",age)
+
+
+let Firstname ="Shweta"; let lastname ="Bholankar"; let Maritialstatus ="Married"; let Country ="India"; 
+let Age = "28"
+
+//Declare two variable my age and your age and assign them initial values and log to the console
+console.log(MyAge)
+let YourAge = 28
+console.log(YourAge)
