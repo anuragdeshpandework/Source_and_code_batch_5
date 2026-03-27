0f960272f22628e3deb3b42b0d510dd6f87c6b8b
Tejas-Pund <tejasspund@gmail.com>
2026-03-27 18:49:09 +0530

    assignment4

---
+++ BATCH_5_STUDENTS/Tejas Pund/JAVASCRIPT/JS_ASSIGNMENTS/ASSIGN_4.js
@@ -0,0 +1,30 @@
+// 1)
+     let greeting = "Hello World"
+     console.log(typeof greeting)
+// 2)
+     let price = 19.99
+     console.log(Math.ceil(price))
+// 3)
+     let name = "Tejas Sanjay Pund"
+     console.log(name.length)
+// 4)
+     let firstName = "Tejas"
+     let middleName = "Sanjay"
+     let lastName = "Pund"
+     console.log(firstName+middleName+lastName)
+     console.log(`${firstName} ${middleName} ${lastName}`)
+// 5)
+     let language = "JavaScript"
+     console.log(language[4])
+// 6)
+     let longLiteralString = "adlnfleadnflknadlfndskjbckjdbkshabvfjhbas\
+     ijadljfja"
+     console.log(longLiteralString)
+// 7)
+// 8)
+// 9)
+// 10)
+// 11)
+// 12)
+// 13)
+// 14)
